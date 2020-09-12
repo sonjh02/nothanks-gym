7bd4260b5f7af28b651cd1ff2ea95935d43cc9a2
Jaehyeon Son <sonjh02@gmail.com>
2020-09-13 01:07:03 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { SocketIOProvider } from 'use-socketio'
 import App from './App'
 
 ReactDOM.render(
-  <SocketIOProvider url="http://localhost:3001">
+  <SocketIOProvider url="http://nothanks.sonjh02.me:3001">
     <RecoilRoot>
       <StrictMode>
         <App />
